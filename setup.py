a21113c2423dc36e19794b078bd9228a8bfbdc4e
zsx <sxzhang1@leqee.com>
2024-04-07 13:37:04 +0800

    规整路径

--- setup.py
+++ setup.py
@@ -6,25 +6,30 @@
 # @日期时间 :2024/4/3 17:06
 # @文件介绍 :
 """
-import setuptools  # 导入setuptools打包工具
+import setuptools
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="zsx_pack",  # 用自己的名替换其中的YOUR_USERNAME_
-    version="0.0.1",  # 包版本号，便于维护版本
-    author="zsx",  # 作者，可以写自己的姓名
-    author_email="17630583910@163.com",  # 作者联系方式，可写自己的邮箱地址
-    description="自用包，主要是为了不行使用logger无法屏蔽第三方包日志",  # 包的简述
-    long_description=long_description,  # 包的详细介绍，一般在README.md文件内
+    name="zsx_pack",
+    version="1.0.0",
+    author="zsx",
+    author_email="17630583910@163.com",
+    description="自用包，主要是为了logger无法屏蔽第三方包日志而开发的",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",  # 自己项目地址，比如github的项目地址
-    packages=setuptools.find_packages(),
+    url="https://github.com/ziyueguyi/zsx_pack",
+    project_urls={
+        "Bug Tracker": "https://github.com/ziyueguyi/zsx_pack",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Natural Language :: Chinese (Simplified)",
     ],
-    python_requires='>=3.6',  # 对python的最低版本要求
+    package_dir={"": "logger"},
+    python_requires=">=3.6",
 )
